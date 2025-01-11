e6c111743bc125d38b687df91a9584e3161033f5
Renato Bosshart <bosshart.renato@gmail.com>
2025-01-11 23:04:16 +0100

    Added Functionalitz

--- PiHome/Ambilight/ScreenReader/ScreenReader.cs
+++ PiHome/Ambilight/ScreenReader/ScreenReader.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Communication.ApiCommunication;
 using HPPH;
 using ScreenCapture.NET;
 
@@ -10,10 +14,17 @@ namespace Ambilight.ScreenReader;
 public class ScreenReader
 {
     private readonly IScreenCaptureService screenCaptureService;
+    private readonly CommunicatorFactory commFac = new();
     private GraphicsCard? graphicsCard;
     private Display? display;
     private IScreenCapture screenCapture;
     private ICaptureZone? zone;
+    private bool isRunning;
+    private double xZone;
+    private double yZone;
+    private double zoneWidth;
+    private double zoneHeight;
+    private CancellationTokenSource? tokenSource;
 
     public IEnumerable<(string Name, int DeviceId)> GraphicCards => screenCaptureService.GetGraphicsCards().Select(x => (x.Name, x.DeviceId));
     public IEnumerable<(string Name, int Index)> Displays => graphicsCard is null ? Enumerable.Empty<(string Name, int Index)>() : screenCaptureService.GetDisplays(graphicsCard.Value).Select(x => (x.DeviceName, x.Index));
@@ -63,12 +74,126 @@ public class ScreenReader
 
     public void SetArea(double x, double y, double width, double height)
     {
-        if (zone is not null)
+        if (isRunning)
         {
-            screenCapture.UnregisterCaptureZone(zone);
+            throw new InvalidOperationException("Running please stop first");
         }
 
-        zone = screenCapture.RegisterCaptureZone((int)(display.Value.Width * x), (int)(display.Value.Height * y),
-            (int)(display.Value.Width * width), (int)(display.Value.Height * height), 2);
+        xZone = (display.Value.Width * x);
+        yZone = (display.Value.Height * y);
+        zoneWidth = (display.Value.Width * width);
+        zoneHeight = (display.Value.Height * height);
+    }
+
+    public void Start(IPAddress ip, int start, int end, byte brightness = 13, bool flip = false)
+    {
+        if (isRunning)
+        {
+            return;
+        }
+        Task.Run(() =>
+        {
+            isRunning = true;
+            tokenSource = new CancellationTokenSource();
+            var comm = commFac.GetLedCommunicator(ip);
+            var sendBuffer = new byte[end * 4];
+            var numLeds = end - start;
+            var sumBuffer = new ulong[numLeds * 3];
+            var rowPerLed = new int[numLeds];
+            for (int i = start; i < end; i++)
+            {
+                sendBuffer[i * 4 + 3] = brightness;
+            }
+            var scale = Math.Log2(zoneWidth / numLeds);
+            zone = screenCapture.RegisterCaptureZone((int)xZone, (int)yZone, (int)zoneWidth, (int)zoneHeight, (int)scale);
+            var colMapping = new int[zone.Width * zone.ColorFormat.BytesPerPixel];
+            var factor = (double)zone.Width / numLeds;
+            for (int i = 0; i < zone.Width; i++)
+            {
+                var ledNbr = (int)(i / factor);
+                switch (zone.ColorFormat)
+                {
+                    case ColorFormatABGR:
+                        colMapping[i * zone.ColorFormat.BytesPerPixel] = -1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 1] = ledNbr * 3 + 2;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 2] = ledNbr * 3 + 1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 3] = ledNbr + 3;
+                        break;
+                    case ColorFormatARGB:
+                        colMapping[i * zone.ColorFormat.BytesPerPixel] = -1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 1] = ledNbr * 3;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 2] = ledNbr * 3 + 1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 3] = ledNbr * 3 + 2;
+                        break;
+                    case ColorFormatBGR:
+                        colMapping[i * zone.ColorFormat.BytesPerPixel] = ledNbr * 3 + 2;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 1] = ledNbr * 3 + 1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 2] = ledNbr * 3;
+                        break;
+                    case ColorFormatBGRA:
+                        colMapping[i * zone.ColorFormat.BytesPerPixel] = ledNbr * 3 + 2;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 1] = ledNbr * 3 + 1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 2] = ledNbr * 3;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 3] = -1;
+                        break;
+                    case ColorFormatRGB:
+                        colMapping[i * zone.ColorFormat.BytesPerPixel] = ledNbr * 3;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 1] = ledNbr * 3 + 1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 2] = ledNbr * 3 + 2;
+                        break;
+                    case ColorFormatRGBA:
+                        colMapping[i * zone.ColorFormat.BytesPerPixel] = ledNbr * 3;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 1] = ledNbr * 3 + 1;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 2] = ledNbr * 3 + 2;
+                        colMapping[i * zone.ColorFormat.BytesPerPixel + 3] = -1;
+                        break;
+
+                }
+                rowPerLed[ledNbr]++;
+            }
+
+            var rowLength = zone.Width * 4;
+            var token = tokenSource.Token;
+            while (!token.IsCancellationRequested)
+            {
+                screenCapture.CaptureScreen();
+                Array.Clear(sumBuffer);
+                using (zone.Lock())
+                {
+                    var raw = zone.RawBuffer;
+                    for (int i = 0; i < raw.Length; i++)
+                    {
+                        var col = i % rowLength;
+                        var target = colMapping[col];
+                        if (target > 0)
+                        {
+                            sumBuffer[target] += raw[i];
+                        }
+                    }
+
+                    for (int i = 0; i < numLeds; i++)
+                    {
+                        var mapped = flip ? end - 1 - i : i + start;
+                        var numValues = (ulong)(zone.Height * rowPerLed[i]);
+                        sendBuffer[mapped * 4] = (byte)(sumBuffer[i * 3] / numValues);
+                        sendBuffer[mapped * 4 + 1] = (byte)(sumBuffer[i * 3 + 1] / numValues);
+                        sendBuffer[mapped * 4 + 2] = (byte)(sumBuffer[i * 3 + 2] / numValues);
+                    }
+
+                    comm.SetRGBB(sendBuffer, false);
+                }
+
+                //await Task.Delay(50);
+            }
+
+            screenCapture.UnregisterCaptureZone(zone);
+            zone = null;
+            tokenSource = null;
+        });
+    }
+
+    public void Stop()
+    {
+        tokenSource?.Cancel();
     }
 }
--- PiHome/Ambilight/ViewModels/MainViewModel.cs
+++ PiHome/Ambilight/ViewModels/MainViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Windows.Input;
 using Avalonia;
@@ -10,19 +9,34 @@ using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using CommunityToolkit.Mvvm.Input;
+using DataPersistance.Models;
+using DataPersistance.Modules;
 
 namespace Ambilight.ViewModels;
 
 public partial class MainViewModel : ViewModelBase, INotifyPropertyChanged
 {
-    public ScreenReader.ScreenReader reader = new();
+    private ScreenReader.ScreenReader reader = new();
+    private ModuleRepository repo = new ModuleRepository(new PiHomeContext());
+
     private (string Name, int DeviceId) selectedCard;
     private ObservableCollection<(string Name, int Index)> displays = new();
     private (string Name, int Index) selectedDisplay;
     private IImage screenPreview = new WriteableBitmap(new PixelSize(100, 100), new Vector(200, 200), PixelFormat.Rgb32, AlphaFormat.Unpremul);
+    private Module? selectedModule;
 
     public string Greeting => "Enable Ambilght!";
     public IList<(string Name, int DeviceId)> GraphicCards => reader.GraphicCards.ToList();
+    public IList<(string Name, int Ip)> Modules => repo.GetAllModules().Select(x => (x.Name, x.Id)).ToList();
+    public int StartIndex { get; set; }
+    public int EndIndex { get; set; }
+    public int Brightness { get; set; }
+    public bool Flip { get; set; }
+    public bool CanRefresh { get; private set; }
+    public bool ShowArea { get; private set; }
+    public bool CanStart => selectedModule is not null && ShowArea && !CanStop;
+    public bool CanStop { get; private set; }
+    public IImage ScreenPreview => screenPreview;
 
     public (string Name, int DeviceId) SelectedCard
     {
@@ -42,6 +56,20 @@ public partial class MainViewModel : ViewModelBase, INotifyPropertyChanged
             }
         }
     }
+    public (string Name, int Id)? SelectedModule
+    {
+        get { return selectedModule is null ? null : (selectedModule.Name, selectedModule.Id); }
+        set
+        {
+            var module = repo.GetModule(value.Value.Id);
+            selectedModule = module;
+            StartIndex = module.Leds.Min(x => x.Index);
+            EndIndex = module.Leds.Max(x => x.Index);
+            OnPropertyChanged(nameof(StartIndex));
+            OnPropertyChanged(nameof(EndIndex));
+            OnPropertyChanged(nameof(CanStart));
+        }
+    }
 
     public ObservableCollection<(string Name, int Index)> Displays => displays;
     public (string Name, int Index) SelectedDisplay
@@ -58,9 +86,6 @@ public partial class MainViewModel : ViewModelBase, INotifyPropertyChanged
         }
     }
 
-    public bool CanRefresh { get; private set; }
-    public bool ShowArea { get; private set; }
-
     public ICommand RefreshCommand => new RelayCommand(() =>
     {
         unsafe
@@ -74,13 +99,28 @@ public partial class MainViewModel : ViewModelBase, INotifyPropertyChanged
         }
     });
 
-    public IImage ScreenPreview => screenPreview;
+    public ICommand StartCommand => new RelayCommand(() =>
+    {
+        reader.Start(selectedModule.Ip, StartIndex, EndIndex, (byte)Brightness, Flip);
+        CanStop = true;
+        OnPropertyChanged(nameof(CanStart));
+        OnPropertyChanged(nameof(CanStop));
+    });
+
+    public ICommand StopCommand => new RelayCommand(() =>
+    {
+        reader.Stop();
+        CanStop = false;
+        OnPropertyChanged(nameof(CanStart));
+        OnPropertyChanged(nameof(CanStop));
+    });
 
     public void AreaSet(double x, double y, double width, double height)
     {
         reader.SetArea(x, y, width, height);
         ShowArea = true;
         OnPropertyChanged(nameof(ShowArea));
+        OnPropertyChanged(nameof(CanStart));
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
